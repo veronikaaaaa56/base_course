32e5ad73c60543c56f3c2e268f1d229d7495407b
veronikaaaaa56 <rajkoveronika@mail.ru>
2025-10-25 13:00:34 +0000

    сделала дз

---
+++ dz_object2.py
@@ -0,0 +1,11 @@
+x="Hello, World!"
+print(x, type(x))
+
+x=3+4
+print(x, type(x))
+
+x=3/4
+print(x, type(x))
+
+x=[1, 2, 5, 10, 100]
+print(x, type(x))
---
+++ lec_math_dz.py
@@ -0,0 +1,7 @@
+print("Hello,World")
+print(type("Маша+Петя=Любовь"))
+
+print(3+4)
+
+x=7
+print(x)
