533c2c35444a23e0aaebdaa2e5ad25fb32ede850
veronikaaaaa56 <rajkoveronika@mail.ru>
2025-11-22 13:29:21 +0000

    лаба 3

---
+++ dz_lab3.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+g = 9.8 
+
+x_0 = 2
+y_0 = 5
+v = 16
+alpha = np.pi / 180 * 45
+vx_0= v * np.cos(alpha)
+vy_0= v * np.sin(alpha)
+
+coords = []
+for t in np.arange (0, 5, 0.1):
+    x = x_0 + vx_0 * t 
+    y = y_0 + vy_0 * t - g * t**2 / 2
+
+    coords.append([t, x , y])
+
+coords = np.array(coords)
+print(coords)
+
+print()
+
+t = np.arange(0, 5, step)
+x = x_0 + vx_0 * t 
+y = y_0 + vy_0 * t - g * t**2/2
+
+coords = np.zeros((len(t), 3))
---
+++ lec_np.array.py
@@ -0,0 +1,18 @@
+	
+import numpy as np
+ 
+a = [1, 2, 4]
+ 
+b = np.array(a) # Создание массива из списка
+ 
+print(type(a))
+print(type(b))
+ 
+print(b * b)
+print(b / b)
+print(b - b)
+ 
+# print(a - a)
+ 
+print(b[-1]) # Вызов последнего элеманта массива
+print(a[-1])
